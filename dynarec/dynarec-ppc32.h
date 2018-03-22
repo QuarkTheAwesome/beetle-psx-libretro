87001676b8a88f51517bbebaf73c8d499d4d0021
Ash <quarktheawesome@gmail.com>
2018-03-23 10:24:58 +1100

    PowerPC: Add headers and guff

--- dynarec/dynarec-ppc32.h
+++ dynarec/dynarec-ppc32.h
@@ -1,3 +1,12 @@
+/* PSX -> 32-bit big-endian PowerPC dynarec.
+   Written based on docs for the 750CL, but should conform to the UISA
+   750CL: https://fail0verflow.com/media/files/ppc_750cl.pdf
+   PEM (referenced by 750CL manual): https://preview.tinyurl.com/ycws6xx9
+
+   Originally by Ash Logan <quarktheawesome@gmail.com>
+   For licensing, see the LICENSE or COPYING file included with this repository.
+*/
+
 #ifndef __DYNAREC_PPC32_H__
 #define __DYNAREC_PPC32_H__
 
